85f45ed5e5a1e03e16ad3dd21fa7f5fcd906e890
roccas28 <roccas.raveendran@torontomu.ca>
2024-06-05 17:44:52 -0400

    changed dataset

--- Data_Profiling.py
+++ Data_Profiling.py
@@ -2,7 +2,7 @@
 import pandas as pd
 from ydata_profiling import ProfileReport
 
-df = pd.read_csv(r'C:\Users\Roccas\Documents\TMU Data\CIND820 Big Data Analytics Project\data_2.csv')
+df = pd.read_csv(r'C:\Users\Roccas\Documents\TMU Data\CIND820 Big Data Analytics Project\diabetes_binary_health_indicators_BRFSS2015.csv')
 profile = ProfileReport(df, title="Profiling Report")
 profile.to_file(r"C:\Users\Roccas\Documents\TMU Data\CIND820 Big Data Analytics Project\ydata_report.html")
 
@@ -13,7 +13,7 @@ import sweetviz as sv
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-data = pd.read_csv(r'C:\Users\Roccas\Documents\TMU Data\CIND820 Big Data Analytics Project\data_2.csv')
+data = pd.read_csv(r'C:\Users\Roccas\Documents\TMU Data\CIND820 Big Data Analytics Project\diabetes_binary_health_indicators_BRFSS2015.csv')
 
 report = sv.analyze(data)
 
